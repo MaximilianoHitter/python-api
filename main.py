69a0c93d5fa9b66fe6fbc19aaabd5ef75dbf5eb1
MaximilianoHitter <mr.vonhumbolt@gmail.com>
2023-09-10 14:35:07 -0300

    añadido de orm sqlalchemy y clases

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
 from fastapi import FastAPI
-import mysql.connector
+#import mysql.connector
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+from databases import Database
 
 app = FastAPI()
+#definicion de db
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@localhost/laravel-react-api"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+database = Database(SQLALCHEMY_DATABASE_URL)
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password='',
-    database="laravel-react-api"
-)
+#clase Trainer 
+class Trainer(Base):
+    __tablename__ = "trainers"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255))
+    last_name = Column(String(255))
+    profile_picture_url = Column(String(255))
+
+#clase Specialist 
+class Specialist(Base):
+    __tablename__ = "specialists"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255))
+    last_name = Column(String(255))
+    profile_picture_url = Column(String(255))
+
+
+#metodos para acceder al orm 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+    Base.metadata.create_all(bind=engine)
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
 
 #rutas 
-@app.get('/')
-def index():
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT name, email FROM users")
-    result = mycursor.fetchall()
-    return {'data':result, 'hola':'hola'}
+@app.get('/trainers')
+async def get_trainers():
+    db = SessionLocal()
+    trainers = db.query(Trainer).all()
+    return {'trainers': trainers}
+
+@app.get('/specialists')
+async def get_specialists():
+    db = SessionLocal()
+    specialists = db.query(Specialist).all()
+    return {'specialists': specialists}
